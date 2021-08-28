bf5e8b36f06b27e37ca965aea80350257414a9e8
bcharlotin1 <biancacharlotin1@gmail.com>
2021-08-28 10:49:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
-const routine = 
-[
-    wakeDog(),
-    leashDog(),
-    walkToPark(),
-    throwFrisbee(),
-    walkHome(),
-    unleashDog(),
-]
+
 
 function wakeDog(dogName, dogBreed) {
     console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`;
   }
+  
 
 function leashDog(dogName, dogBreed) {
     console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`;
   }
 
 function walkToPark(dogName, dogBreed) {
     console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
   }
 
 function throwFrisbee(dogName, dogBreed) {
     console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
   }
 
 function walkHome(dogName, dogBreed) {
     console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`
   }
 
 function unleashDog(dogName, dogBreed) {
     console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`;
   }
 
-
+  const routine = 
+  [
+      wakeDog,
+      leashDog,
+      walkToPark,
+      throwFrisbee,
+      walkHome,
+      unleashDog,
+  ]
 
 function exerciseDog(dogName, dogBreed){
-    newArray = []
+    // const newArray = routine.map(function(func){
+    //     // function here is ana anonymous function 
+    //     return func(dogName, dogBreed);
+    // })
+ const newArray = routine.map(func => func(dogName, dogBreed)
+    //  func is a place holder  representing all the other functions in the rution array
+    // we want creat a function or do something with the  for that pace holder to tell it what to do 
+    
+ )
+//     newArray = []
 
-  for(let i = 0; i < routine.length; i++){
-      callFunction = routine[i];
-      newArray.push(callFunction(dogName, dogBreed));
+// //   for(let i = 0; i < routine.length; i++){
+// //       callFunction = routine[i];
+// //       newArray.push(callFunction(dogName, dogBreed));
 
-  }
   return newArray
 }
+
+exerciseDog(dog, breed)
